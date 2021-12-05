9fa91a34f3c1440536bb640395c30d776cc49e12
mandar <sakpalm466@gmail.com>
2021-12-05 10:53:28 +0530

    connected to firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app"
+
+const Config={
+    apiKey: "AIzaSyBHIDcSCwBb387cyFrckw-zf3DubP-4LuE",
+    authDomain: "chat-web-app-65843.firebaseapp.com",
+    projectId: "chat-web-app-65843",
+    storageBucket: "chat-web-app-65843.appspot.com",
+    messagingSenderId: "379625912056",
+    appId: "1:379625912056:web:b1e6ba14a8520eefbf6d0d"
+  };
+
+  const app= firebase.initializeApp(Config);
+  
